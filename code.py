3537b87410033451da41bd1a69d64a54a7a8fd70
dyoun04 <141276127+dyoun04@users.noreply.github.com>
2023-12-02 12:40:28 -0500

    Add files via upload

--- code.py
+++ code.py
@@ -6,6 +6,7 @@ import board
 from adafruit_seesaw import seesaw, rotaryio, digitalio
 import adafruit_character_lcd.character_lcd_i2c as character_lcd
 import time
+import random
 
 
 #Wifi and Thingspeak stuff
@@ -23,23 +24,72 @@ def GenerateColors():
     neoFun.bar_graph((255,255,0), 17, True, 12) #yellow
     neoFun.bar_graph((0,191,255), 24, True, 18) #blue
 
-def Flash(flashvalue, color1, color2, color3, end_pos, start_pos):
+def Flash(flashvalue, color_index):
+        color_list = [[0,191,255,24,18], [0,200,0,5,0], [255,0,0,11,6], [255,255,0,17,12]]
         if flashvalue:
-            neoFun.bar_graph((0,0,0), end_pos, True, start_pos)
+            neoFun.bar_graph((0,0,0), color_list[color_index][3], True, color_list[color_index][4])
         else:
-            neoFun.bar_graph((color1, color2,color3), end_pos, True, start_pos)
+            neoFun.bar_graph(tuple(color_list[color_index][0:3]), color_list[color_index][3], True, color_list[color_index][4])
 
 
-def Flash_3(color1, color2, color3, end_pos, start_pos):
-        for i in range(3):
-            neoFun.bar_graph((0,0,0), end_pos, True, start_pos)
-            time.sleep(.1)
-            neoFun.bar_graph((color1, color2,color3), end_pos, True, start_pos)
-            time.sleep(.1)
+def Flash_3(color_index):
+        color_list = [[0,191,255,24,18], [0,200,0,5,0], [255,0,0,11,6], [255,255,0,17,12]]
+        for i in range(1):
+            time.sleep(.25)
+            neoFun.bar_graph(tuple(color_list[color_index][0:3]), color_list[color_index][3], True, color_list[color_index][4])
+            time.sleep(.25)
+            neoFun.bar_graph((0,0,0), color_list[color_index][3], True, color_list[color_index][4])
+
+def next_color():
+    return random.randint(0,3)
+
+def match_sequence(color_sequence):
+    color_select = 0
+    success = True
+    position = -encoder.position
+    button_state = button.value
+    button_prev = button_state
+    prev_position = position
+    prev_time = time.monotonic()
+    flash_on = False
+
+    for color in color_sequence:
+        if not success:
+            break
+        while(success):
+            position = -encoder.position
+            if position != prev_position:
+                if (prev_position > position): ## if counterclockwise
+                    GenerateColors()
+                    color_select -= 1
+                    color_select %= 4
+                else:
+                    GenerateColors()
+                    color_select += 1
+                    color_select %= 4
+                prev_position = position
+                prev_time = time.monotonic() - 0.25
+                flash_on = False
+
+            if time.monotonic() - prev_time > .25: #in terms of milseconds
+                prev_time = time.monotonic()
+                flash_on = not flash_on
+                Flash(flash_on,color_select)
+
+            button_state = button.value
+            if not button_state and button_prev:
+                if color_select != eval(color):
+                    success = False
+                else:
+                    button_prev = button_state
+                    break
+            button_prev = button_state
+    return success
 
 hostServer = 'http://134.82.159.191:8000'
-joinPath = '/join.json'
-rotaryPath = '/rotary.json'
+joinPath = '/join'
+startPath = '/start'
+scorePath = '/scores'
 
 ################################################################# Startup Code for Rotary Encoder ##############################################################
 I2C = board.I2C()
@@ -47,9 +97,6 @@ seesaw = seesaw.Seesaw(I2C, addr=0x36)
 seesaw.pin_mode(24, seesaw.INPUT_PULLUP)
 button = digitalio.DigitalIO(seesaw,24)
 encoder = rotaryio.IncrementalEncoder(seesaw)
-
-button_prev = button.value
-button_state = button_prev
 
 ################################################################# Startup Code for Rotary Encoder ##############################################################
 cols = 16
@@ -70,19 +117,12 @@ GenerateColors()
 motor_tool = motorFun.ECEGMotor(I2C)
 
 #Calling this will automaticly make the motor try and find its home(i.e. try to make a full rotation and end up hitting the stop peg)
-motor_tool.find_home()
+#motor_tool.find_home()
 
 ################################################################# Main Loop #################################################################
 
-color_list = [[0,191,255,24,18], [0,200,0,5,0], [255,0,0,11,6], [255,255,0,17,12]] #
-combination_list = []
-sample_game_bool = True
-sample_game = [0,1,2,3,0]
-list_position = 0
-starttime = 0
-flash_on = False
-color_list_value = None
-prev_position = 0
+color_index = 0
+color_sequence = ''
 
 lcd.backlight = True
 lcd.message = 'Connecting...'
@@ -90,48 +130,51 @@ net_tool = espFun.ESP_Tools(NETWORK_NAME, NETWORK_PASS)
 lcd.clear()
 lcd.message = 'Connected to\nnetwork!'
 
+playerID = 0
+gameID = 0
+scores = {'1' : 0, '2' : 0}
+
+txt = net_tool.api_get(hostServer + joinPath)
+playerID = txt[-2:-1]
+lcd.clear()
+lcd.message = txt
+
+# If player 1, wait for player 2. Otherwise, start game
+if(playerID == '1'):
+    lcd.clear()
+    lcd.message = 'Waiting for\nPlayer 2...'
+
+    while(True):
+        time.sleep(3)
+        gameID = json.loads(net_tool.api_get(hostServer + startPath))['id']
+        if(gameID != 0):
+            break
+else:
+    net_tool.api_post(hostServer + startPath, data='start')
+    gameID = json.loads(net_tool.api_get(hostServer + startPath))['id']
+random.seed(gameID)
+
+lcd.clear()
+lcd.message = f'Player 1: {str(scores["1"])}\nPlayer 2: {str(scores["2"])}'
 
 while(True):
 
-    if sample_game_bool:
-        for i in range(len(sample_game)):
-            # print(sample_game[i])
-            Flash_3(color_list[sample_game[i]][0], color_list[sample_game[i]][1], color_list[sample_game[i]][2], color_list[sample_game[i]][3], color_list[sample_game[i]][4])
-        sample_game_bool = False
+    neoFun.set_ring_color((0,0,0))
+    time.sleep(0.5)
+    color_sequence += str(next_color())
+    for color in color_sequence:
+        Flash_3(eval(color))
+    time.sleep(0.5)
+    GenerateColors()
 
-    if sample_game_bool == False:
+    if match_sequence(color_sequence):
+        scores[playerID] += 1
+    else:
+        neoFun.set_ring_color((255,0,0))
+        break
 
-        button_state = button.value
-        if button_prev == False and button_state == True and color_list_value != None:
-            combination_list.append(idx)
-            print(combination_list) ## will push this list to the LCD screen to make sure they have enough
-            net_tool.api_post(hostServer + rotaryPath, data=str(combination_list))
-            if len(combination_list) == 5:
-                if combination_list == sample_game:
-                    print("correct")
-                else:
-                    print("incorrect")
-                combination_list = []
-                GenerateColors()
-                break
-        button_prev = button_state
+    net_tool.api_post(hostServer + scorePath, data=str(str(playerID) + ' ' + str(scores[playerID])))
+    scores = json.loads(net_tool.api_get(hostServer + scorePath))
+    print(scores)
 
-
-        position = -encoder.position
-        if position != prev_position:
-            if (prev_position > position): ## if counterclockwise
-                list_position -= 1
-                GenerateColors()
-                idx = list_position % 4
-                color_list_value = color_list[idx]
-            else:
-                list_position += 1
-                GenerateColors()
-                idx = list_position % 4
-                color_list_value = color_list[idx]
-            prev_position = position
-
-        if time.monotonic() - starttime > .25 and color_list_value != None: #in terms of milseconds
-            starttime = time.monotonic()
-            flash_on = not flash_on
-            Flash(flash_on,color_list_value[0], color_list_value[1], color_list_value[2], color_list_value[3], color_list_value[4])
+    lcd.message = f'Player 1: {str(scores["1"])}\nPlayer 2: {str(scores["2"])}'
